8e0aed8d688cae2537a213cea07f09fafc1d42ff
declared-as-ala <alamissaoui.dev@gmail.com>
2026-07-04 04:45:28 +0100

    hh

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,29 @@
+import { type DefaultSession } from "next-auth"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id?: string
+      role?: string
+    } & DefaultSession["user"]
+  }
+
+  interface User {
+    id?: string
+    role?: string
+  }
+}
+
+declare module "@auth/core/types" {
+  interface Session {
+    user: {
+      id?: string
+      role?: string
+    } & DefaultSession["user"]
+  }
+
+  interface User {
+    id?: string
+    role?: string
+  }
+}
